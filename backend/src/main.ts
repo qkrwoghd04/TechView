cf9b2d7f98dbd5b293ca45af3f5e7ef0532cbc8f
qkrwoghd04 <qkrwoghd04@naver.com>
2025-08-21 21:40:38 +0900

    add: cor 출처에 jaehong.link 추가

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://jaehong.link'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
   });
